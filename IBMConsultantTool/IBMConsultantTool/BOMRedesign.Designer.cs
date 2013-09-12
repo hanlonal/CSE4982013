5ae3002d578f7232fb81fc0fc8a659d7b4155dcb
Kingofhearts102 <koshore4@msu.edu>
2013-09-11 21:43:32 -0700

    Added new BOM tool layout. Rough but works
    We dont have to use this, but I think it adds a cool style to the
    program. everyone let me know what you think

---
+++ IBMConsultantTool/IBMConsultantTool/BOMRedesign.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace IBMConsultantTool
+{
+    partial class BOMRedesign
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddBox = new System.Windows.Forms.Button();
+            this.CategoryBox = new System.Windows.Forms.ComboBox();
+            this.mainWorkspace = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // AddBox
+            // 
+            this.AddBox.Location = new System.Drawing.Point(626, 30);
+            this.AddBox.Name = "AddBox";
+            this.AddBox.Size = new System.Drawing.Size(75, 23);
+            this.AddBox.TabIndex = 0;
+            this.AddBox.Text = "Add ";
+            this.AddBox.UseVisualStyleBackColor = true;
+            this.AddBox.Click += new System.EventHandler(this.AddBox_Click);
+            // 
+            // CategoryBox
+            // 
+            this.CategoryBox.FormattingEnabled = true;
+            this.CategoryBox.Items.AddRange(new object[] {
+            "Products",
+            "Catergory A",
+            "Category B",
+            "Category C"});
+            this.CategoryBox.Location = new System.Drawing.Point(589, 73);
+            this.CategoryBox.Name = "CategoryBox";
+            this.CategoryBox.Size = new System.Drawing.Size(121, 21);
+            this.CategoryBox.TabIndex = 1;
+            this.CategoryBox.Text = "Select Category";
+            this.CategoryBox.SelectedIndexChanged += new System.EventHandler(this.CategoryBox_SelectedIndexChanged);
+            // 
+            // mainWorkspace
+            // 
+            this.mainWorkspace.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.mainWorkspace.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.mainWorkspace.Location = new System.Drawing.Point(12, 30);
+            this.mainWorkspace.Name = "mainWorkspace";
+            this.mainWorkspace.Size = new System.Drawing.Size(563, 204);
+            this.mainWorkspace.TabIndex = 2;
+            // 
+            // BOMRedesign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 261);
+            this.Controls.Add(this.mainWorkspace);
+            this.Controls.Add(this.CategoryBox);
+            this.Controls.Add(this.AddBox);
+            this.Name = "BOMRedesign";
+            this.Text = "BOMRedesign";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddBox;
+        private System.Windows.Forms.ComboBox CategoryBox;
+        private System.Windows.Forms.Panel mainWorkspace;
+
+    }
+}
---
+++ IBMConsultantTool/IBMConsultantTool/BOMRedesign.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IBMConsultantTool
+{
+    public partial class BOMRedesign : Form
+    {
+        List<Panel> categoryPanels = new List<Panel>();
+        List<Color> colors = new List<Color>();
+        private int categoryCount = 0;
+
+        public BOMRedesign()
+        {
+            PopulateColorsList();
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void CategoryBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddBox_Click(object sender, EventArgs e)
+        {
+
+            categoryCount++;
+            Panel dynamicPanel = new Panel();
+
+            dynamicPanel.Location = new System.Drawing.Point(0, 0);
+
+            dynamicPanel.Name = CategoryBox.Text;
+
+            dynamicPanel.Size = new System.Drawing.Size(100, 100);
+
+            dynamicPanel.BackColor = colors[categoryCount-1];
+            mainWorkspace.Controls.Add(dynamicPanel);
+            dynamicPanel.Visible = true;
+
+            Label categoryLabel = new Label();
+            categoryLabel.BorderStyle = BorderStyle.Fixed3D;
+            categoryLabel.Location = new System.Drawing.Point(0, 0);
+            categoryLabel.Text = CategoryBox.Text;
+            
+            dynamicPanel.Controls.Add(categoryLabel);
+            dynamicPanel.Click +=new EventHandler(dynamicPanel_Click);
+            dynamicPanel.MouseMove +=new MouseEventHandler(dynamicPanel_MouseMove);
+
+            categoryPanels.Add(dynamicPanel);
+
+        }
+
+        private void dynamicPanel_Click(object sender, EventArgs e)
+        {
+            Panel a = (Panel)sender;
+
+            
+            Console.WriteLine(a.Location.ToString());
+        }
+        private void dynamicPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Panel panel = (Panel)sender;
+            if (e.Button == MouseButtons.Left)
+            {
+                panel.Left += e.X;
+                panel.Top += e.Y;
+                Console.WriteLine(panel.Name);
+            }
+        }
+
+        private void PopulateColorsList()
+        {
+            colors.Add(Color.Red);
+            colors.Add(Color.Blue);
+            colors.Add(Color.Pink);
+            colors.Add(Color.Green);
+            colors.Add(Color.Lavender);
+            colors.Add(Color.Salmon);
+            colors.Add(Color.Ivory);
+        }
+ 
+
+
+
+
+    }
+}
