0955b62120b6c8a6001478bf51fdba3f9e9390b4
Connor Gilbert <connorsname@gmail.com>
2013-10-23 00:36:08 -0400

    Added multiple survey reading in BOM

--- IBMConsultantTool/IBMConsultantTool/SurveyReader.cs
+++ IBMConsultantTool/IBMConsultantTool/SurveyReader.cs
@@ -19,24 +19,96 @@ namespace IBMConsultantTool
 
         }
 
-        public void ReadSurvey(string file, List<NewCategory> BomCats)
+        public string TruncateLongString(string str, int maxLength)
         {
+            return str.Substring(0, Math.Min(str.Length, maxLength));
+        }
+
+        public void ReadSurvey(List<NewCategory> BomCats)
+        {
+            var FD = new System.Windows.Forms.FolderBrowserDialog();
+            if (FD.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+
+            var files = Directory.EnumerateFiles(FD.SelectedPath);
+            var badFiles = 0;
+            foreach (var file in files)
+            {
+
+                if (file.Contains("~$")) 
+                {
+                    badFiles++;
+                    continue;
+                }
             //Start Word and open the word document.
             Word._Application oWord;
             Word._Document oDoc;
             oWord = new Word.Application();
             oWord.Visible = false;
 
-            oDoc = oWord.Documents.Open(file, Type.Missing, Type.Missing, Type.Missing, 
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                Type.Missing, Type.Missing, false, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-            //oWord.Activate();
+            
+                oDoc = oWord.Documents.Open(file, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, false, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                //oWord.Activate();
 
-            foreach ( Word.FormField form in oDoc.FormFields)
+                //This is going to be ugly.
+                foreach (Word.FormField form in oDoc.FormFields)
+                {
+                    //compare name of form to each category/obj/initiative that exists
+                    //if theres a match set the correct value
+                    foreach (NewCategory cat in BomCats)
+                    {
+                        foreach (NewObjective obj in cat.Objectives)
+                        {
+                            foreach (NewInitiative ini in obj.Initiatives)
+                            {
+                                string BaseIniName = TruncateLongString(cat.Name, 5) +
+                                TruncateLongString(obj.Name, 5) +
+                                TruncateLongString(ini.Name, 5);
+                                string temp = BaseIniName + "Diff";
+                                if (temp == form.Name)
+                                {
+                                    ini.Differentiation += Convert.ToSingle(form.Result);
+                                }
+                                temp = BaseIniName + "Eff";
+                                if (temp == form.Name)
+                                {
+                                    ini.Effectiveness += Convert.ToSingle(form.Result);
+                                }
+                                temp = BaseIniName + "Crit";
+                                if (temp == form.Name)
+                                {
+                                    ini.Criticality += Convert.ToSingle(form.Result);
+                                }
+                            }
+                        }
+                    }
+                }
+                oDoc.Close(Word.WdSaveOptions.wdDoNotSaveChanges);
+            }
+
+
+            foreach (NewCategory cat in BomCats)
             {
-                var value = form.Result;
+                foreach (NewObjective obj in cat.Objectives)
+                {
+                    foreach (NewInitiative ini in obj.Initiatives)
+                    {
+                        var temp = Convert.ToSingle(files.Count()) - badFiles;
+                        ini.Effectiveness = ini.Effectiveness / temp;
+                        ini.Criticality = ini.Criticality / temp;
+                        ini.Differentiation = ini.Differentiation / temp;
+                    }
+                }
             }
 
         }
+
+
+
+
     }
 }
