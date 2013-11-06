b098c4d88dd8424b52db75dc6f0f9a45e99f91b5
Kingofhearts102 <koshore4@msu.edu>
2013-11-05 19:09:34 -0500

    trends

--- IBMConsultantTool/IBMConsultantTool/CUPEQuestionTrendAnalysis.cs
+++ IBMConsultantTool/IBMConsultantTool/CUPEQuestionTrendAnalysis.cs
@@ -8,6 +8,29 @@ namespace IBMConsultantTool
     public class CUPEQuestionTrendAnalysis : TrendAnalysisEntity
     {
         //four answers
+
+        private string cupeType;
+
+        public string CupeType
+        {
+            get { return cupeType; }
+            set { cupeType = value; }
+        }
+        private float currentAnswer = 0;
+
+        public float CurrentAnswer
+        {
+            get { return currentAnswer; }
+            set { currentAnswer = value; }
+        }
+        private float futureAnswer = 0;
+
+        public float FutureAnswer
+        {
+            get { return futureAnswer; }
+            set { futureAnswer = value; }
+        }
+
         public string Name
         {
             get { return name; }
