8fbb821d88cf07a2765f32a7d3433c6d097ea64a
Kingofhearts102 <koshore4@msu.edu>
2013-11-09 14:42:52 -0500

    bom detailed view

--- IBMConsultantTool/IBMConsultantTool/DetailedBOMViewForm.cs
+++ IBMConsultantTool/IBMConsultantTool/DetailedBOMViewForm.cs
@@ -18,11 +18,33 @@ namespace IBMConsultantTool
             InitializeComponent();
             this.owner = obj;
             dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersHeight = 50;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.LightSteelBlue;
+            businessObjectiveName.Text = obj.Name;
+            
         }
 
         private void DetailedBOMViewForm_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = owner.Initiatives;
+            dataGridView1.DataSource = owner.Initiatives[0];
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dataGridView1.Columns["Name"].Width = 200;
+            dataGridView1.Columns["TotalBOMScore"].HeaderText = "Weighed ECD Score \n\n ((11-EFF)*(CRIT*.5))/10 \n +DIFF*0.5";
+            dataGridView1.Columns["TotalBOMScore"].Width = 190;
+            dataGridView1.Columns["Effectiveness"].Width = 120;
+            dataGridView1.Columns["Differentiation"].Width = 120;
+
+            dataGridView1.Columns["TotalBOMScore"].DefaultCellStyle.BackColor = Color.LightYellow;
+            dataGridView1.Columns["Differentiation"].DefaultCellStyle.BackColor = Color.AliceBlue;
+            dataGridView1.Columns["Effectiveness"].DefaultCellStyle.BackColor = Color.AliceBlue;
+            dataGridView1.Columns["Criticality"].DefaultCellStyle.BackColor = Color.AliceBlue;
+
+
         }
     }
 }
