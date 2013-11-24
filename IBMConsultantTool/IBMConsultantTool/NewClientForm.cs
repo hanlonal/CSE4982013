67d65800e476cbe9f0e71842d9a0b6dc77dcf435
eunbong <yangeunb@msu.edu>
2013-11-24 14:34:23 -0500

    File Path choose when client makes

--- IBMConsultantTool/IBMConsultantTool/NewClientForm.cs
+++ IBMConsultantTool/IBMConsultantTool/NewClientForm.cs
@@ -6,12 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace IBMConsultantTool
 {
     public partial class NewClientForm : Form
     {
         DateTime selectedTime;
+        private string selectedCloseString = "OK";
 
         public NewClientForm()
         {
@@ -21,12 +23,14 @@ namespace IBMConsultantTool
         private void NewClientForm_Load(object sender, EventArgs e)
         {
             this.BringToFront();
-            okButton.Click +=new EventHandler(okButton_Click);
-            cancelButton.Click +=new EventHandler(cancelButton_Click);
-            startDateText.Click +=new EventHandler(startDateText_Click);
+            okButton.Click += new EventHandler(okButton_Click);
+            cancelButton.Click += new EventHandler(cancelButton_Click);
+            startDateText.Click += new EventHandler(startDateText_Click);
             //countryComboBox.Items.AddRange(ClientDataControl.GetCoutnryNames());
             RegionComboBox.Items.AddRange(ClientDataControl.GetRegionNames());
             BusinessTypeComboBox.Items.AddRange(ClientDataControl.GetBusinessTypeNames());
+
+            this.FormClosed += new FormClosedEventHandler(NewClientForm_FormClosed);
         }
 
 
@@ -60,13 +64,14 @@ namespace IBMConsultantTool
 
         private void cancelButton_Click(object sender, EventArgs e)
         {
+            selectedCloseString = "close";
             this.Close();
         }
         private void startDateText_Click(object ender, EventArgs e)
         {
             MonthCalendar date = new MonthCalendar();
             this.Controls.Add(date);
-            date.DateSelected +=new DateRangeEventHandler(date_DateSelected);
+            date.DateSelected += new DateRangeEventHandler(date_DateSelected);
             date.Visible = true;
             date.BringToFront();
             //date.MinDate = new System.DateTime
@@ -74,7 +79,7 @@ namespace IBMConsultantTool
         private void date_DateSelected(object sender, DateRangeEventArgs e)
         {
             MonthCalendar cal = (MonthCalendar)sender;
-            
+
             startDateText.Text = e.Start.Date.ToShortDateString();
             selectedTime = e.Start.Date;
             cal.Visible = false;
@@ -95,6 +100,32 @@ namespace IBMConsultantTool
             countryComboBox.Items.Clear();
             countryComboBox.Text = "";
             countryComboBox.Items.AddRange(ClientDataControl.db.GetCountryNames(RegionComboBox.Text).ToArray());
+        }
+
+        private void NewClientForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (selectedCloseString == "OK")
+            {
+                //System.Diagnostics.Trace.WriteLine("Name: " + ClientDataControl.Client.Name);
+                //string path = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());//.CreateDirectory(ClientDataControl.Client.Name);
+                //path = Directory.GetDirectoryRoot(path);
+                string path = "C:\\User\\Desktop\\" + ClientDataControl.Client.Name;
+                Directory.CreateDirectory(@"C:\\User\\Desktop\\" + ClientDataControl.Client.Name);
+                Directory.CreateDirectory(path + "\\Charts");
+
+
+                FolderBrowserDialog openFileDialog1 = new FolderBrowserDialog();
+
+                DialogResult userClickedOK = openFileDialog1.ShowDialog();
+
+                if (userClickedOK == DialogResult.OK)
+                {
+                    ClientDataControl.Client.FilePath = openFileDialog1.SelectedPath.ToString();
+                    System.Diagnostics.Trace.WriteLine("path: " + openFileDialog1.SelectedPath.ToString());
+                    //MessageBox.Show("Charts will save in the path you selected.\nYou will find your charts' images in the folder you selected.");
+                    //System.IO.Path.GetDirectoryName().ToString();
+                }
+            }
         }
 
     }
